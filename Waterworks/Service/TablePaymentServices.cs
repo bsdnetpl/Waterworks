438226880668e369484abea54cad591e62cce590
Adrian U <99587723+bsdnetpl@users.noreply.github.com>
2023-07-31 16:12:58 +0200

    add payment

---
+++ Waterworks/Service/TablePaymentServices.cs
@@ -0,0 +1,28 @@
+﻿using Waterworks.DB;
+using Waterworks.DTO;
+
+namespace Waterworks.Service
+{
+    public class TablePaymentServices
+    {
+        private readonly ConnectMssql _connectMssql;
+
+        public TablePaymentServices(ConnectMssql connectMssql)
+        {
+            _connectMssql = connectMssql;
+        }
+
+        public bool AddPayment(TablePayment tablePayment)
+        {
+            _connectMssql.tablePayments.Add(tablePayment);
+            return true;
+        }
+        public bool DeletePayment(Guid IdPyment)
+        {
+            var result = _connectMssql.tablePayments.Find(IdPyment);
+            _connectMssql.tablePayments.Remove(result);
+            _connectMssql.SaveChanges();
+            return true;
+        }
+    }
+}
